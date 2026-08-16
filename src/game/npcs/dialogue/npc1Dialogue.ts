b28d2aa5d0e98355e97814da85e6d4665220b7b6
Astro-Syn <kelseyamyb@gmail.com>
2026-08-15 20:52:55 -0400

    start on dialogue

---
+++ src/game/npcs/dialogue/npc1Dialogue.ts
@@ -0,0 +1,13 @@
+import Phaser from "phaser";
+
+export const npc1Dialogue = [
+    [
+    "Whats an old man like me know anyways?",
+    "Where did she go?",
+    "Huh?"
+    ],
+    [
+        "Where the fuck is my hat?",
+        "The moon was just following me"
+    ]
+]
