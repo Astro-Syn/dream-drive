6fdad3244db8fd7768b465c85494eaab0fb41580
Astro-Syn <kelseyamyb@gmail.com>
2026-08-22 17:50:32 -0400

    update title screen animation

--- src/titleScreen/TitleScreen.ts
+++ src/titleScreen/TitleScreen.ts
@@ -17,6 +17,15 @@ export default class TitleScreen extends Phaser.Scene {
             "/Images/title-screen-letters.png"
         );
 
+        this.load.spritesheet(
+            "dd-title-animation",
+            "/Images/dd-title-sprite-sheet.png",
+            {
+                frameWidth: 669,
+                frameHeight: 117
+            }
+        )
+
         this.load.image(
             "title-screen-moon",
             "/Images/title-screen-moon.png"
@@ -42,8 +51,42 @@ export default class TitleScreen extends Phaser.Scene {
         //moon
         this.add.image(180, 140, "title-screen-moon").setOrigin(0.5);
 
-        //Game title
-        this.add.image(400, 280, "title-screen-title").setOrigin(0.5);
+       
+
+
+        //title animation
+
+        this.anims.create({
+            key: "dd-title-animation",
+            frames: this.anims.generateFrameNumbers(
+                "dd-title-animation",
+                {
+                    start: 0,
+                    end: 9
+                }
+            ),
+            frameRate: 12,
+            repeat: 0
+        })
+
+        const titleAnimation = this.add.sprite(
+            400, 280,
+            "dd-title-animation"
+        );
+
+
+      titleAnimation.on(
+    Phaser.Animations.Events.ANIMATION_COMPLETE,
+    () => {
+        this.time.delayedCall(7000, () => {
+            titleAnimation.play("dd-title-animation");
+        });
+    }
+);
+
+        
+        titleAnimation.play("dd-title-animation");
+       
 
         //start button
         const startButton = this.add.text(400, 450, "Start",
